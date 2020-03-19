f03ef304efb0b50fa32f5c5005abceff448578aa
maaz.hassan <h.maaz809@hotmail.com>
2020-03-19 17:59:41 -0600

    Project structure update.

--- SortingVisualizer/src/algorithms/bubble_sort.js
+++ SortingVisualizer/src/algorithms/bubble_sort.js
@@ -1,16 +1,5 @@
 'use strict';
-
-//Generate a random number in a range
-let randomRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-//Generate a random array
-function randomArray(size, min, max) {
-    let array = [];
-    for (let i = 0; i < size; i++) {
-        array.push(randomRange(min, max));
-    }
-    return array;
-}
+import { randomArray } from "./bubble_sort"
 
 let a = randomArray(50, 5, 500);
 console.log(a);
---
+++ SortingVisualizer/src/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Generate a random number in a range
+export let randomRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+//Generate a random array
+export function randomArray(size, min, max) {
+    let array = [];
+    for (let i = 0; i < size; i++) {
+        array.push(randomRange(min, max));
+    }
+    return array;
+}
