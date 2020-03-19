c9930f27c596d5f24a1805548f9c2fcabbd92063
maaz.hassan <h.maaz809@hotmail.com>
2020-03-19 17:30:49 -0600

    Added bubble sort algorithm.

--- algorithms/bubble_sort.js
+++ algorithms/bubble_sort.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function randomRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+//Generate a random number in a range
+let randomRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
+//Generate a random array
 function randomArray(size, min, max) {
     let array = [];
     for (let i = 0; i < size; i++) {
@@ -12,5 +12,23 @@ function randomArray(size, min, max) {
     return array;
 }
 
-let a = randomArray(30, 3, 20);
+let a = randomArray(50, 5, 500);
 console.log(a);
+
+//Bubble sort algorithm
+function bubbleSort(arr) {
+    let n = arr.length - 1;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n-i; j++) {
+            if (arr[j] > arr[j+1]) {
+                let num = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = num;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(a));
