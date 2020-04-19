cf2a30e7414c288d5dd84d5f33b2302510072d1d
maaz.hassan <h.maaz809@hotmail.com>
2020-04-18 20:52:42 -0600

    Basic page structure/functionality.

--- SortingVisualizer/src/app.js
+++ SortingVisualizer/src/app.js
@@ -19,12 +19,101 @@ function randomArray(size, min, max) {
     return array;
 }
 
-let arr = randomArray(50, 5, 500);
+//SVG element in index.html
+let svg = document.getElementById("rects");
 
-console.log(bubbleSort(arr));
-console.log(heapSort(arr));
-console.log(insertionSort(arr));
-console.log(mergeSort(arr));
-console.log(quickSortH(arr, 0, arr.length-1));
-console.log(quickSortL(arr, 0, arr.length-1));
-console.log(selectionSort(arr));
+//Function that displays the rectangles for the array elements
+function displayArray(arr) {
+    let width = 500/arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        let rec = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        rec.setAttribute("x", i*width)
+        rec.setAttribute("width", width);
+        rec.setAttribute("height", arr[i]);
+        rec.setAttribute("style", "fill:rgb(0, 0, 0)");
+        rec.setAttribute("stroke-width", "0.3");
+        rec.setAttribute("stroke", "white");
+        svg.appendChild(rec);
+    }
+}
+
+//Function to clear
+function clear() {
+    let rectsList = svg.querySelectorAll("rect");
+    rectsList.forEach(r => {
+        svg.removeChild(r);
+    });
+}
+
+//Initial array/draw
+let arr = randomArray(50, 5, 500);
+displayArray(arr);
+
+//Slider element in index.html
+let slider = document.getElementById("myRange");
+slider.step = "5";
+
+//Updating rects when slider moves
+slider.oninput = function() {
+    clear();
+    arr = randomArray(this.value, 5, 500);
+    displayArray(arr);
+}
+
+//Button elements from index.html
+let bubbleSortButton = document.getElementById("bubbleSort");
+let heapSortButton = document.getElementById("heapSort");
+let insertionSortButton = document.getElementById("insertionSort");
+let mergeSortButton = document.getElementById("mergeSort");
+let quickSortHButton = document.getElementById("quickSortH");
+let quickSortLButton = document.getElementById("quickSortL");
+let selectionSortButton = document.getElementById("selectionSort");
+let sort = document.getElementById("sortButton");
+
+let label = document.getElementById("algo-name");
+
+//Default algorithm
+let selectedAlgo = bubbleSort; 
+
+//Event handlers for algorithm buttons
+bubbleSortButton.onclick = function() {
+    selectedAlgo = bubbleSort;
+    label.innerHTML = "Bubble Sort";
+}
+
+heapSortButton.onclick = function() {
+    selectedAlgo = heapSort;
+    label.innerHTML = "Heap Sort";
+}
+
+insertionSortButton.onclick = function() {
+    selectedAlgo = insertionSort;
+    label.innerHTML = "Insertion Sort";
+}
+
+mergeSortButton.onclick = function() {
+    selectedAlgo = mergeSort;
+    label.innerHTML = "Merge Sort";
+}
+
+quickSortHButton.onclick = function() {
+    selectedAlgo = quickSortH;
+    label.innerHTML = "Quick Sort (Hoare)";
+}
+
+quickSortLButton.onclick = function() {
+    selectedAlgo = quickSortL;
+    label.innerHTML = "Quick Sort (Lomuto)";
+}
+
+selectionSortButton.onclick = function() {
+    selectedAlgo = selectionSort;
+    label.innerHTML = "Selection Sort";
+}
+
+//Event handler for sort button
+sort.onclick = function() {
+    clear();
+    arr = (selectedAlgo === quickSortH || selectedAlgo === quickSortL) ? selectedAlgo(arr, 0, arr.length-1) : selectedAlgo(arr);
+    displayArray(arr);
+}
